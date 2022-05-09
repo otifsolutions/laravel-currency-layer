6b7fcb66cfd51a5f3850ad21b1c5d38bc0a76568
danish9811 <danish.mehmood@otif-solutions.com>
2022-05-09 13:18:11 +0500

    made tiny changes in provider

--- src/CurrencyLayerServiceProvider.php
+++ src/CurrencyLayerServiceProvider.php
@@ -16,20 +16,11 @@ class CurrencyLayerServiceProvider extends ServiceProvider {
             $schedule->command('rates:delete')->daily()->at('08:00');
         });
 
-//        $this->publishes([
-//            __DIR__ . '/../database/seeders/CitySeeder.php' => database_path('seeders/CitySeeder.php'),
-//        ], 'otifsolutions-seeder');
-
-        if ($this->app->runningInConsole()) {
-            $this->publishResources();
-        }
-
-
-//        include __DIR__ . '/../src/database/seeders/CountrySeeder.php';
-//        include __DIR__ . '/../src/database/seeders/TimezoneSeeder.php';
-//        include __DIR__ . '/../src/database/seeders/StateSeeder.php';
-//        include __DIR__ . '/../src/database/seeders/CitySeeder.php';
-//        include __DIR__ . '/../src/database/seeders/CurrencySeeder.php';
+        include __DIR__ . '/../src/database/seeders/CountrySeeder.php';
+        include __DIR__ . '/../src/database/seeders/TimezoneSeeder.php';
+        include __DIR__ . '/../src/database/seeders/StateSeeder.php';
+        include __DIR__ . '/../src/database/seeders/CitySeeder.php';
+        include __DIR__ . '/../src/database/seeders/CurrencySeeder.php';
 
         $this->publishes([
             __DIR__ . '/../public' => public_path('flags/'),
@@ -47,25 +38,5 @@ class CurrencyLayerServiceProvider extends ServiceProvider {
             ]);
         }
     }
-
-    private function publishResources() {
-        $this->publishes([
-            __DIR__ . '/../database/seeders/CitySeeder.php' => database_path('seeders/CitySeeder.php'),
-        ], 'otifsolutions-seeder');
-        $this->publishes([
-            __DIR__ . '/../database/seeders/CountrySeeder.php' => database_path('seeders/CountrySeeder.php'),
-        ], 'otifsolutions-seeder');
-        $this->publishes([
-            __DIR__ . '/../database/seeders/CurrencySeeder.php' => database_path('seeders/CurrencySeeder.php'),
-        ], 'otifsolutions-seeder');
-        $this->publishes([
-            __DIR__ . '/../database/seeders/StateSeeder.php' => database_path('seeders/StateSeeder.php'),
-        ], 'otifsolutions-seeder');
-        $this->publishes([
-            __DIR__ . '/../database/seeders/TimezoneSeeder.php' => database_path('seeders/TimezoneSeeder.php'),
-        ], 'otifsolutions-seeder');
-
-    }
-
 
 }
