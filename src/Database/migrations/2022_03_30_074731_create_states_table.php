105c8492a3a15c2cdf4382ed6d4d624d653236fe
danish9811 <danish.mehmood@otif-solutions.com>
2022-05-17 19:20:14 +0500

    removed country_code colomn from state migration

--- src/Database/migrations/2022_03_30_074731_create_states_table.php
+++ src/Database/migrations/2022_03_30_074731_create_states_table.php
@@ -15,7 +15,6 @@ return new class extends Migration {
             $table->engine = 'myIsam';
             $table->id();
             $table->string('name');             // state name like "badakhshan"
-            $table->string('country_code');     // country code like "AF" for afghanistan
             $table->string('state_code');       // state code like "BDS" for badakhshan
             $table->foreignId('country_id')->references('id')->on('countries');
             $table->float('latitude')->nullable();
