6ecb11d42a9a4c929a25a353d8880d8970e27b50
danish9811 <danish.mehmood@otif-solutions.com>
2022-05-24 17:36:10 +0500

    re-written checks

--- src/Commands/RemoveHistoricalRates.php
+++ src/Commands/RemoveHistoricalRates.php
@@ -10,26 +10,28 @@ use OTIFSolutions\Laravel\Settings\Models\Setting;
 class RemoveHistoricalRates extends Command {
 
     protected $signature = 'rates:delete';
-    protected $description = 'Will remove or partially remove the exchange rates from exchange rates table';
+    protected $description = 'Command will remove older data from rates_days days';
 
     public function handle() {
-        $accessKey = Setting::get('crkey');
 
-        if (!isset($accessKey)) {
-            $this->warn('accessKey is not set. Set it first and re-run the command');
+        $accessKey = Setting::get('crlKey');
+        $ratesSaveDays = Setting::get('daysRates');   // data of how many days we want to store
+
+        if (!isset($accessKey, $ratesSaveDays)) {
+            $this->warn('Either accessy key \'crlKey\' or rates save days \'daysRates\' is not set. Check it how README.md');
             return;
         }
-
-        $ratesSaveDays = Setting::get('days_rates');   // data of how many days we want to store
 
         if (!(is_numeric($ratesSaveDays) && is_numeric(abs($ratesSaveDays)))) {
-            $this->warn('numDays should be a positive integer and not a character string');
+            $this->warn('daysRates key should be a positive integer');
             return;
         }
+
 
         if (CurrencyRate::exists()) {
             CurrencyRate::whereDate('created_at', '<=', Carbon::now()->subDays($ratesSaveDays))->delete();
             $this->info('exchange rates synced successfully');
+            $this->newLine();
         }
 
     }
