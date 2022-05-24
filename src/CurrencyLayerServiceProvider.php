8079702145d0a5eaeb5326c5ab79369f550a697d
danish9811 <danish.mehmood@otif-solutions.com>
2022-05-24 17:28:36 +0500

    added two command classes and method to publish resources/flags

--- src/CurrencyLayerServiceProvider.php
+++ src/CurrencyLayerServiceProvider.php
@@ -5,8 +5,9 @@ namespace OTIFSolutions\CurrencyLayer;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Support\ServiceProvider;
 use OTIFSolutions\CurrencyLayer\Commands\FetchCurrencyRates;
+use OTIFSolutions\CurrencyLayer\Commands\FillAllDatabaseTables;
+use OTIFSolutions\CurrencyLayer\Commands\PublishResources;
 use OTIFSolutions\CurrencyLayer\Commands\RemoveHistoricalRates;
-use OTIFSolutions\CurrencyLayer\Commands\CallPackageSeeders;
 
 class CurrencyLayerServiceProvider extends ServiceProvider {
 
@@ -26,11 +27,15 @@ class CurrencyLayerServiceProvider extends ServiceProvider {
             $this->commands([
                 FetchCurrencyRates::class,
                 RemoveHistoricalRates::class,
-                CallPackageSeeders::class
+                FillAllDatabaseTables::class,
+                PublishResources::class
             ]);
 
+            $this->publishResources();
         }
+    }
 
+    public function publishResources() {
         $this->publishes([
             __DIR__ . '/public/flags' => public_path('flags/')
         ], 'otif-flags');
