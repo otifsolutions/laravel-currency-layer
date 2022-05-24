ec3131e54365edaff02abdc1b02372c6feb9b318
danish9811 <danish.mehmood@otif-solutions.com>
2022-05-24 17:35:12 +0500

    command added to publish countries flags at public/flags

--- src/Commands/PublishResources.php
+++ src/Commands/PublishResources.php
@@ -1,23 +1,20 @@
 <?php
 
-namespace App\Console\Commands;
+namespace OTIFSolutions\CurrencyLayer\Commands;
 
-use Artisan;
 use Illuminate\Console\Command;
 
 class PublishResources extends Command {
-    protected $signature = 'publish:resources';
+
+    protected $signature = 'publish:flags';
 
     protected $description = 'Publish all the resources of currency layer package into your project';
 
     public function handle() {
 
-        //Artisan::call('php artisan vendor:publish --tag=otif-flags');
-
-        // when we call artisan froma anywhere other than artisan console, we don't write 'php artisan'
         Artisan::call('vendor:publish --tag=otif-flags');
 
-        $this->line('flags published to public/flags');
+        $this->info('flags published to public/flags');
         $this->newLine();
 
         return 0;
